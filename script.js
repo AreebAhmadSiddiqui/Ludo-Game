2dbe418641dd850119d597e8dc1d167fe2c3b6dd
Areeb Ahmad Siddiqui <87814983+AreebAhmadSiddiqui@users.noreply.github.com>
2022-02-09 12:17:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let button=document.querySelector(".btn")
 button.addEventListener("click",()=>{
     let p1=Math.floor(Math.random()*6)+1
     let p2=Math.floor(Math.random()*6)+1
-    document.querySelector("#img-1").src=`Images/${p1}.png`;
-    document.querySelector("#img-2").src=`Images/${p2}.png`;
+    document.querySelector("#img-1").src=`images/${p1}.png`;
+    document.querySelector("#img-2").src=`images/${p2}.png`;
     if(p1>p2){
        document.querySelector("h1").innerHTML="Player 1 Won"
     }
