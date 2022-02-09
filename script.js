f1f3d053ed15d4c482f897495c1d1a21db1174aa
Areeb Ahmad Siddiqui <sareebahmad@gmail.com>
2022-02-09 12:10:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let button=document.querySelector(".btn")
+button.addEventListener("click",()=>{
+    let p1=Math.floor(Math.random()*6)+1
+    let p2=Math.floor(Math.random()*6)+1
+    document.querySelector("#img-1").src=`Images/${p1}.png`;
+    document.querySelector("#img-2").src=`Images/${p2}.png`;
+    if(p1>p2){
+       document.querySelector("h1").innerHTML="Player 1 Won"
+    }
+    else if(p1<p2){
+        document.querySelector("h1").innerHTML="Player 2 Won"
+    }
+    else{
+        document.querySelector("h1").innerHTML="Draw"
+    }
+})
